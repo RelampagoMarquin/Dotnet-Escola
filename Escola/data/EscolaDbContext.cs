f29d0e411fc31454345e946581121bb2d72d4f5d
Marcos Moreira <marcos_more@outlook.com>
2023-06-05 14:22:13 -0300

    add DataContextExtasion to context

--- Escola/data/EscolaDbContext.cs
+++ Escola/data/EscolaDbContext.cs
@@ -9,8 +9,26 @@ namespace Escola.data
             
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.LowercaseRelationalTableANdPropertyNames();
+        }
         public DbSet<Aluno>? Alunos {get; set;}
         public DbSet<Curso>? Cursos {get; set;}
         public DbSet<Disciplina>? Disciplinas {get; set;}
     }
+
+    static class DataContextExtensions{
+        public static void LowercaseRelationalTableANdPropertyNames(this ModelBuilder modelBuilder){
+            foreach (var entity in modelBuilder.Model.GetEntityTypes()){
+                entity.SetTableName(entity.GetTableName().ToLowerInvariant());
+                foreach (var property in entity.GetProperties()){
+                    property.GetColumnName().ToLowerInvariant();
+                }
+            }
+        }
+    }
 }
+ 
